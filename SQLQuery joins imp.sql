4d88c664339b25a050a3f0bd315fbbbf875ad840
M. Dhanush Prasad <dhanushprasad27@gmail.com>
2025-09-01 20:05:38 +0530

    Update SQLQuery joins.sql

--- SQLQuery joins imp.sql
+++ SQLQuery joins imp.sql
@@ -1,22 +1,23 @@
---joins statemnt in sql
---select * from SalesLT.SalesOrderDetail;
+joins statemnt in sql
+Select * from SalesLT.SalesOrderDetail;
 
---Select * from SalesLT.Product;
+Select * from SalesLT.Product;
 
---select * from SalesLT.SalesOrderDetail as sod inner join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
---select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod inner join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+select * from SalesLT.SalesOrderDetail as sod inner join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod inner join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
 
 --left ouetr join
---select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod left join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+Select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod left join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
 --full outer join
---select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod full outer join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod full outer join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
 --union 
---select ProductID from SalesLT.SalesOrderDetail
---union
---Select ProductID from SalesLT.Product
+select ProductID from SalesLT.SalesOrderDetail
+  union
+Select ProductID from SalesLT.Product
 
 --cross join
 select * from SalesLT.SalesOrderDetail cross join SalesLT.Product
+
 
 
 
