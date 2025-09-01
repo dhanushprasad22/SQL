d1bf5fef8d09253326a94e37e3f88ef8d8d78552
M. Dhanush Prasad <dhanushprasad27@gmail.com>
2025-09-01 14:59:18 +0530

    Add files via upload

---
+++ SQLQuery joins imp.sql
@@ -0,0 +1,22 @@
+--joins statemnt in sql
+--select * from SalesLT.SalesOrderDetail;
+
+--Select * from SalesLT.Product;
+
+--select * from SalesLT.SalesOrderDetail as sod inner join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+--select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod inner join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+
+--left ouetr join
+--select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod left join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+--full outer join
+--select sod.SalesOrderID,sod.SalesOrderDetailID, prd.Name, prd.DiscontinuedDate from SalesLT.SalesOrderDetail as sod full outer join SalesLT.Product as prd on sod.ProductID = prd.ProductID;
+--union 
+--select ProductID from SalesLT.SalesOrderDetail
+--union
+--Select ProductID from SalesLT.Product
+
+--cross join
+select * from SalesLT.SalesOrderDetail cross join SalesLT.Product
+
+
+
